caafe0ed0b0a255a65059b8a45436a3a265114f5
wisygig <jason.mcgibbon@gmail.com>
2017-06-15 23:54:41 -0400

    Initial replacement of NA values

--- etl.py
+++ etl.py
@@ -1,13 +1,74 @@
 import json
+import numpy as np
+import pandas as pd
 import requests
+
+_stats = ['strength', 'dexterity', 'constitution',
+         'intelligence', 'wisdom', 'charisma']
 
 
 def main():
+    monsters, ogl = get_monsters()
+    monster_df = get_monster_df(monsters)
+    return monster_df
+
+
+def get_monsters():
     url = 'https://dl.dropboxusercontent.com/s/iwz112i0bxp2n4a/5e-SRD-Monsters.json'
     data = json.loads(requests.get(url).text)
     monsters = data[:-1]
     ogl = data[-1]
-    return monsters
+    return monsters, ogl
+
+
+def get_monster_df(monsters):
+    df = pd.DataFrame(monsters)
+    df = df.set_index(['name'])
+    df = fix_saves(df)
+    df = fix_skills(df)
+    seq = ['challenge_rating', 'armor_class', 'hit_dice', 'hit_points',
+       'condition_immunities',
+       'damage_immunities', 'damage_resistances', 'damage_vulnerabilities',
+       'actions', 'reactions', 'legendary_actions', 'special_abilities',
+       'size', 'speed',
+       'languages', 'senses', 'subtype', 'type', 'alignment',
+       'strength', 'strength_mod', 'strength_save', 'dexterity', 'dexterity_mod', 'dexterity_save', 'constitution', 'constitution_mod', 'constitution_save', 'intelligence', 'intelligence_mod', 'intelligence_save', 'wisdom', 'wisdom_mod', 'wisdom_save', 'charisma', 'charisma_mod', 'charisma_save', 'acrobatics', 'arcana', 'athletics', 'deception', 'history',  'insight', 'intimidation', 'investigation', 'medicine', 'nature', 'perception', 'performance',  'persuasion', 'religion', 'stealth', 'survival',
+       ]
+    return df.reindex(columns=seq)
+
+
+
+def fix_saves(df):
+    mods = [stat + '_mod' for stat in _stats]
+    saves = [stat + '_save' for stat in _stats]
+    for stat, mod in zip(_stats, mods):
+        df[mod] = np.floor((df[stat] -10 )/ 2)
+    for mod, save in zip(mods, saves):
+        df[save].fillna(df[mod], inplace=True)
+    return df
+
+
+def fix_skills(df):
+    skills = {'acrobatics': 'dexterity',
+              'arcana': 'intelligence',
+              'athletics': 'strength',
+              'deception': 'charisma',
+              'history': 'intelligence',
+              'insight': 'wisdom',
+              'intimidation': 'charisma',
+              'investigation': 'intelligence',
+              'medicine': 'wisdom',
+              'nature': 'intelligence',
+              'perception': 'wisdom',
+              'performance': 'charisma',
+              'persuasion': 'charisma',
+              'religion': 'intelligence',
+              'stealth': 'dexterity',
+              'survival': 'wisdom'}
+    for skill, stat in skills.items():
+        df[skill].fillna(df[stat+'_mod'], inplace=True)
+    return df
+
 
 if __name__ == '__main__':
     main()
